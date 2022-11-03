a0b6b6cda91eb46f5285af55e2facd7e53100ff5
NaNa923 <k5279kr@gmail.com>
2022-11-03 16:08:17 +0900

    json경로수정

--- src/components/sections/Ourwork.js
+++ src/components/sections/Ourwork.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import workData from 'https://nana923.github.io/pitapat_2020/data/workData.json'
+import workData from '../../data/workData.json'
 
 const Ourwork = ({sections}) => {
 
@@ -40,7 +40,7 @@ const Ourwork = ({sections}) => {
                     }
                 </div>
                 <button className="more" style={style} onClick={()=>{
-                    axios.get('https://nana923.github.io/pitapat_2020/data/workData.json')
+                    axios.get('http://nana923.github.io/pitapat_2020/data/workData.json')
                     .then((result)=>{
                         setWork([...work,...result.data.list1]);
                         setStyle({display: 'none'});
