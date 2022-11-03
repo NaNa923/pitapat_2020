ccc82aeb4ae80a032929d44d48668d937513cbcc
NaHyeon Kim <88807442+NaNa923@users.noreply.github.com>
2022-11-03 15:28:10 +0900

    Update Ourwork.js

--- src/components/sections/Ourwork.js
+++ src/components/sections/Ourwork.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import workData from '../data/workData.json'
+import workData from '../../data/workData.json'
 
 const Ourwork = ({sections}) => {
 
@@ -40,7 +40,7 @@ const Ourwork = ({sections}) => {
                     }
                 </div>
                 <button className="more" style={style} onClick={()=>{
-                    axios.get('../data/workData.json')
+                    axios.get('../../data/workData.json')
                     .then((result)=>{
                         setWork([...work,...result.data.list1]);
                         setStyle({display: 'none'});
